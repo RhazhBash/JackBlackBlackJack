eea460511dee6d1835a41f6d2deab7fffe23a719
daniel fisher <dfishersc@gmail.com>
2021-10-14 16:46:02 -0500

    pls lord let this work

--- FrontEnd/Front/src/app/app.module.ts
+++ FrontEnd/Front/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { QuoteBubbleComponent } from './quote-bubble/quote-bubble.component';
+import { RulesPopUpComponent } from './rules-pop-up/rules-pop-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    QuoteBubbleComponent,
+    RulesPopUpComponent
   ],
   imports: [
     BrowserModule,
---
+++ FrontEnd/Front/src/app/quote-bubble/quote-bubble.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quote-bubble',
+  templateUrl: './quote-bubble.component.html',
+  styleUrls: ['./quote-bubble.component.css']
+})
+export class QuoteBubbleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
