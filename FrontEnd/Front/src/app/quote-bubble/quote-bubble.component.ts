84649ed8c6083f877a13a08c7f81d54ddc203ff3
Curt Burdette <clburdette@yahoo.com>
2021-10-15 13:18:21 -0700

    incomplete quote component for Dan merge back to main

--- FrontEnd/Front/src/app/quote-bubble/quote-bubble.component.ts
+++ FrontEnd/Front/src/app/quote-bubble/quote-bubble.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteBubbleComponent implements OnInit {
 
+  visible = false;
+  quote = "";
+
   constructor() { }
 
   ngOnInit(): void {
