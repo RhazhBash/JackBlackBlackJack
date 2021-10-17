b6dcec57b361a21244d139d2a079675954143468
Andre Montenegro <ajmc200@gmail.com>
2021-10-17 15:18:16 -0400

    userDAO & UserService & userDAOTest stuff

---
+++ src/main/java/com/revature/services/UserService.java
@@ -0,0 +1,15 @@
+package com.revature.services;
+
+import com.revature.daos.userDAO;
+
+public class UserService {
+	
+	userDAO ud = new userDAO();
+	
+	public int getChipCount(int id) {
+		
+		
+		
+		return 5;
+	}
+}
