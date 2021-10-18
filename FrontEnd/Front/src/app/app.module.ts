936233d6d96787730468234c30a0846a66a72d7a
Curt Burdette <clburdette@yahoo.com>
2021-10-18 14:18:35 -0700

    cross your fingers. app.module.ts disaster incoming

--- FrontEnd/Front/src/app/app.module.ts
+++ FrontEnd/Front/src/app/app.module.ts
@@ -9,7 +9,9 @@ import { QuoteBubbleComponent } from './quote-bubble/quote-bubble.component';
 import { RulesPopUpComponent } from './rules-pop-up/rules-pop-up.component';
 import { GameplayviewComponent } from './gameplayview/gameplayview.component';
 import { BsDropdownModule} from 'ngx-bootstrap/dropdown';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { PileService } from './pile.service';
+import { CardComponent}  from './card/card/card.component';
 
 
 
@@ -19,7 +21,8 @@ import { HttpClientModule } from '@angular/common/http'
     NavBarComponent,
     QuoteBubbleComponent,
     RulesPopUpComponent,
-    GameplayviewComponent
+    GameplayviewComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
@@ -29,7 +32,9 @@ import { HttpClientModule } from '@angular/common/http'
     HttpClientModule
     
   ],
-  providers: [],
+  providers: [
+    PileService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
