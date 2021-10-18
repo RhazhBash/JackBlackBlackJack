65081c2a44e82ee7be832e880d0f9e56899257d3
unknown <rhazhbash@gmail.com>
2021-10-18 16:11:31 -0400

    Last night's work

--- src/main/java/com/revature/daos/GameDAO.java
+++ src/main/java/com/revature/daos/GameDAO.java
@@ -1,7 +1,10 @@
 package com.revature.daos;
 
+import java.util.List;
+
 import org.hibernate.Session;
 
+import com.revature.models.Employee;
 import com.revature.models.Game;
 import com.revature.utils.HibernateUtil;
 
@@ -9,19 +12,47 @@ public class GameDAO {
 
 	public boolean newGame(Game game) {
 		//Take the game passed from the service layer and store it in the games table
-		//Create and delete methods return a boolean to signify that it was stored successfully
+		//Return a boolean to confirm it was successfully stored
+		boolean isSuccessful;
 		Session ses = HibernateUtil.getSession();
-		ses.save(game);
+		if (ses!=null) {
+			ses.save(game);
+			isSuccessful=true;
+		}
+		else
+			isSuccessful=false;
 		
 		return isSuccessful;
 	}
 	
-	public Game getGame(int id) {
+	public Game getGame(String id) {
 		//Pass in a game ID and return the game object
+		Session ses = HibernateUtil.getSession();
+		List<Game> gameList = ses.createQuery("FROM gamesession").list();
+		
+		for (int i=0; i<gameList.size(); i++) {
+			Game temp = gameList.get(i);
+			if (temp.getId().equals(id))
+				return temp;
+		}
+		System.out.println("Error");
+		return null;
 	}
 	
-	public boolean deleteGame(int id) {
+	public boolean deleteGame(String id) {
 		//Delete a game with the specified ID, then return a boolean telling you if it was successful
+		Session ses = HibernateUtil.getSession();
+		if (ses!=null) {
+			Game game = (Game)ses.load(Game.class,id);
+			ses.delete(game);
+			ses.flush();
+			return true;
+		}
+		else {
+			System.out.println("Error");
+			return false;
+		}
+		
 	}
 	
 }
--- src/main/java/com/revature/services/GameService.java
+++ src/main/java/com/revature/services/GameService.java
@@ -1,17 +1,23 @@
 package com.revature.services;
+import com.revature.daos.GameDAO;
 //Delete this comment
 import com.revature.models.Game;
-import com.revature.models.Hand;
+
 
 public class GameService {
 	
-	public void startGame(Game game) {
+	GameDAO GDAO = new GameDAO();
+	
+	public String startGame(Game game) {
 		//Take a game object passed from the handler and send it to the DAO to be stored
 		//This method returns the ID of the game for the front end to track
+		GDAO.newGame(game);
+		return game.getId();
 	}
 	
 	public boolean bet(int bet) {
 		//Subtract a player's bet from their chip total
+		//Dependent on userDAO
 	}
 	
 	public void payout(int bet, boolean blackjack) {
@@ -22,9 +28,10 @@ public class GameService {
 		
 	}
 	
-	public void endGame(int id) {
+	public void endGame(String id) {
 		//Delete a game from the table
 		//Anything else we need to do at the end of the game can go in here
+		GDAO.deleteGame(id);
 	}
 	
 }
