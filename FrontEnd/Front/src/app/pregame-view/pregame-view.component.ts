960ff0eedf220ae00fc43d8739e4b1f1441d966d
daniel fisher <dfishersc@gmail.com>
2021-10-22 13:57:05 -0500

    pushing up for andre

--- FrontEnd/Front/src/app/pregame-view/pregame-view.component.ts
+++ FrontEnd/Front/src/app/pregame-view/pregame-view.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DeckService } from '../deck.service';
 
 @Component({
   selector: 'app-pregame-view',
@@ -9,7 +11,7 @@ export class PregameViewComponent implements OnInit {
 
   
 
-  constructor() { }
+  constructor(private router:Router, private deckService:DeckService) { }
 
   ngOnInit(): void {
   }
