5077a32d64416ae23782c4a3370394f445128181
unknown <rhazhbash@gmail.com>
2021-10-24 12:40:27 -0400

    Updated GameDTO

--- src/main/java/com/revature/models/Card.java
+++ src/main/java/com/revature/models/Card.java
@@ -6,11 +6,11 @@ public class Card {
 
 	private String code;
     private String image;
-    private String[] images;
+    private DaCardObject images;
     private String value;
     private String suit;
     
-	public Card(String code, String image, String[] images, String value, String suit) {
+	public Card(String code, String image, DaCardObject images, String value, String suit) {
 		super();
 		this.code = code;
 		this.image = image;
@@ -21,58 +21,6 @@ public class Card {
 
 	public Card() {
 		super();
-	}
-
-	@Override
-	public String toString() {
-		return "Card [code=" + code + ", image=" + image + ", images=" + Arrays.toString(images) + ", value=" + value
-				+ ", suit=" + suit + "]";
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((code == null) ? 0 : code.hashCode());
-		result = prime * result + ((image == null) ? 0 : image.hashCode());
-		result = prime * result + Arrays.hashCode(images);
-		result = prime * result + ((suit == null) ? 0 : suit.hashCode());
-		result = prime * result + ((value == null) ? 0 : value.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Card other = (Card) obj;
-		if (code == null) {
-			if (other.code != null)
-				return false;
-		} else if (!code.equals(other.code))
-			return false;
-		if (image == null) {
-			if (other.image != null)
-				return false;
-		} else if (!image.equals(other.image))
-			return false;
-		if (!Arrays.equals(images, other.images))
-			return false;
-		if (suit == null) {
-			if (other.suit != null)
-				return false;
-		} else if (!suit.equals(other.suit))
-			return false;
-		if (value == null) {
-			if (other.value != null)
-				return false;
-		} else if (!value.equals(other.value))
-			return false;
-		return true;
 	}
 
 	public String getCode() {
@@ -91,11 +39,11 @@ public class Card {
 		this.image = image;
 	}
 
-	public String[] getImages() {
+	public DaCardObject getImages() {
 		return images;
 	}
 
-	public void setImages(String[] images) {
+	public void setImages(DaCardObject images) {
 		this.images = images;
 	}
 
@@ -114,6 +62,8 @@ public class Card {
 	public void setSuit(String suit) {
 		this.suit = suit;
 	}
+    
+	
     
     
 	
