17eea9a16d09f1144e646bbc34d803456df82098
Curt Burdette <clburdette@yahoo.com>
2021-10-24 17:00:49 -0700

    commit testing and changes to playing section to branch

--- FrontEnd/Front/src/app/quote-bubble/quote-bubble.component.spec.ts
+++ FrontEnd/Front/src/app/quote-bubble/quote-bubble.component.spec.ts
@@ -22,4 +22,15 @@ describe('QuoteBubbleComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should return default quote value', () => {
+    let defaultQuote:string = "Maybe I should just quit and go back to making noodles.";
+    expect(component.getQuote()).toEqual(defaultQuote);
+  });
+
+  it('should set new quote value', () => {
+    let testQuote:string = "This is not the greatest song in world. This is just a tribute.";
+    component.setQuote(testQuote);
+    expect(component.getQuote()).toEqual(testQuote);
+  });
 });
