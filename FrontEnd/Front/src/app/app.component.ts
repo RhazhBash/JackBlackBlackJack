27d8b7eb06e7a444a18c125ed71ef54a9b49ff2e
Curt Burdette <clburdette@yahoo.com>
2021-10-25 21:21:52 -0700

    html, css, bootstrap styling changes from 1996 to 2006

--- FrontEnd/Front/src/app/app.component.ts
+++ FrontEnd/Front/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = "Jack Black's Black Jack";
+
+  constructor(private titleService:Title){
+
+  }
+
+  ngOnInit(){
+    this.titleService.setTitle(this.title);
+  }
 }
